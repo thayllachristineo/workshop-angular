8d130d5be2efc6dc25ce2993e2f421292ea9ccff
thaylla <thayllachristineo@gmail.com>
2024-05-14 12:12:27 -0300

    move to nx

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+/* loadComponent: loads the component with lazy loading */
+
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'home',
+    loadComponent: () =>
+      import('./home/home.component').then(
+        (component) => component.HomeComponent
+      ),
+  },
+  {
+    path: 'users',
+    loadComponent: () =>
+      import('./users/users.component').then(
+        (component) => component.UsersComponent
+      ),
+  },
+];
